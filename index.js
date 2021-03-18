ee7cbaecd1db710e0f36f42d102147750e4200d6
bclarke329 <bmendoza329@hotmail.com>
2021-03-17 21:22:42 -0500

    commit tree da70e1ef75f7cde637be9241d5f057c957f1da9b

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(string) {
+ let time = parseInt(string, 10);
+  if (time < 12) {
+    return "Good Morning"
+  } else if (time > 12 && time < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(string){
+  const greeting = document.getElementById('greeting')
+  greeting.innerText = 'TEST'
+}
